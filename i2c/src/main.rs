eb5f857e4bc7e807e53da72038de4bbce2afcb35
Shane <shane@chewielabs.com>
2022-10-10 14:10:28 -0700

    i2c example working

--- i2c/src/main.rs
+++ i2c/src/main.rs
@@ -1,73 +1,31 @@
-//! I2C test with SSD1306
-//!
-//! Folowing pins are used:
-//! SDA     GPIO5
-//! SCL     GPIO6
-//!
-//! Depending on your target and the board you are using you have to change the pins.
-//!
-//! For this example you need to hook up an SSD1306 I2C display.
-//! The display will flash black and white.
-
-use esp_idf_hal::delay::{FreeRtos, BLOCK};
-use esp_idf_hal::i2c::*;
-use esp_idf_hal::peripherals::Peripherals;
+use embedded_hal::blocking::i2c::{Read, Write};
+use esp_idf_hal::i2c;
 use esp_idf_hal::prelude::*;
+use esp_idf_hal::units::FromValueType;
+use esp_idf_sys as _;
+use std::thread;
+use std::time::Duration;
 
-const SSD1306_ADDRESS: u8 = 0x3c;
-
-fn main() -> anyhow::Result<()> {
+const SX1509_i2c_addr: u8 = 0x3E;
+fn main() {
+    // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
+    // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
     esp_idf_sys::link_patches();
+    esp_idf_svc::log::EspLogger::initialize_default();
+    let per = Peripherals::take().unwrap();
+    let sda = per.pins.gpio4.into_input_output().unwrap();
+    let scl = per.pins.gpio5.into_output().unwrap();
+    let i2c = per.i2c0;
 
-    let peripherals = Peripherals::take().unwrap();
-    let i2c = peripherals.i2c0;
-    let sda = peripherals.pins.gpio5;
-    let scl = peripherals.pins.gpio6;
-
-    println!("Starting I2C SSD1306 test");
-
-    let config = config::MasterConfig::new().baudrate(100.kHz().into());
-    let mut i2c = I2cMasterDriver::new(i2c, sda, scl, &config)?;
-
-    // initialze the display - don't worry about the meaning of these bytes - it's specific to SSD1306
-    i2c.write(SSD1306_ADDRESS, &[0, 0xae], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0xd4], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0x80], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0xa8], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0x3f], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0xd3], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0x00], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0x40], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0x8d], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0x14], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0xa1], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0xc8], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0xda], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0x12], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0x81], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0xcf], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0xf1], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0xdb], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0x40], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0xa4], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0xa6], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0xaf], BLOCK)?;
-    i2c.write(SSD1306_ADDRESS, &[0, 0x20, 0x00], BLOCK)?;
-
-    // fill the display
-    for _ in 0..64 {
-        let data: [u8; 17] = [
-            0x40, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-            0xff, 0xff, 0xff,
-        ];
-        i2c.write(SSD1306_ADDRESS, &data, BLOCK)?;
-    }
+    let config = <i2c::config::MasterConfig as Default>::default().baudrate(400.kHz().into());
+    let mut i2cdev =
+        i2c::Master::<i2c::I2C0, _, _>::new(i2c, i2c::MasterPins { sda, scl }, config).unwrap();
 
     loop {
-        // we are sleeping here to make sure the watchdog isn't triggered
-        FreeRtos::delay_ms(500);
-        i2c.write(SSD1306_ADDRESS, &[0, 0xa6], BLOCK)?;
-        FreeRtos::delay_ms(500);
-        i2c.write(SSD1306_ADDRESS, &[0, 0xa7], BLOCK)?;
+        let mut buff: [u8; 6] = [0; 6];
+        i2cdev.write(SX1509_i2c_addr, &[0x11]).unwrap();
+        i2cdev.read(SX1509_i2c_addr, &mut buff).unwrap();
+        log::info!("wai value is {:?}", buff);
+        thread::sleep(Duration::from_millis(100));
     }
 }
